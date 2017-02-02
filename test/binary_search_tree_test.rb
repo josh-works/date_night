6b05214ccc73806d83c8a5078f3e7ba57b7ce78c
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-02 10:11:09 -0700

    rubocop fixes

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -4,7 +4,6 @@ require 'minitest/autorun'
 require_relative '../lib/binary_search_tree'
 require_relative '../lib/node'
 
-
 class DateNightTest < Minitest::Test
   def test_object_is_binary_search_tree
     tree = BinarySearchTree.new
@@ -13,57 +12,57 @@ class DateNightTest < Minitest::Test
 
   def test_has_root_node
     tree = BinarySearchTree.new
-    tree.insert(50, "movie")
+    tree.insert(50, 'movie')
     assert tree.root_node
     assert_equal 50, tree.root_node.score
-    assert_equal tree.root_node.title, "movie"
+    assert_equal tree.root_node.title, 'movie'
   end
 
   def test_insert_root_node_has_right_node
     tree = BinarySearchTree.new
-    tree.insert(50, "movie")
-    tree.insert(60, "larger")
-    assert_equal tree.root_node.right.title, "larger"
+    tree.insert(50, 'movie')
+    tree.insert(60, 'larger')
+    assert_equal tree.root_node.right.title, 'larger'
   end
 
   def test_insert_root_node_has_left_node
     tree = BinarySearchTree.new
-    tree.insert(50, "movie")
-    tree.insert(40, "smaller")
-    assert_equal tree.root_node.left.title, "smaller"
+    tree.insert(50, 'movie')
+    tree.insert(40, 'smaller')
+    assert_equal tree.root_node.left.title, 'smaller'
   end
 
   def test_insert_root_node_has_left_and_right_nodes
     tree = BinarySearchTree.new
-    tree.insert(50, "movie")
-    tree.insert(40, "smaller")
-    tree.insert(60, "larger")
-    assert_equal tree.root_node.left.title, "smaller"
-    assert_equal tree.root_node.right.title, "larger"
+    tree.insert(50, 'movie')
+    tree.insert(40, 'smaller')
+    tree.insert(60, 'larger')
+    assert_equal tree.root_node.left.title, 'smaller'
+    assert_equal tree.root_node.right.title, 'larger'
   end
 
   def test_left_node_has_right_node
     tree = BinarySearchTree.new
-    tree.insert(50, "root")
-    tree.insert(60, "root-right")
-    tree.insert(40, "root-left")
-    tree.insert(45, "root-left-right")
-    tree.insert(35, "root-left-left")
-    tree.insert(46, "root-right-right")
-    tree.insert(36, "root-left-left-right")
-    assert_equal "root", tree.root_node.title
-    assert_equal "root-left-right", tree.root_node.left.right.title
+    tree.insert(50, 'root')
+    tree.insert(60, 'root-right')
+    tree.insert(40, 'root-left')
+    tree.insert(45, 'root-left-right')
+    tree.insert(35, 'root-left-left')
+    tree.insert(46, 'root-right-right')
+    tree.insert(36, 'root-left-left-right')
+    assert_equal 'root', tree.root_node.title
+    assert_equal 'root-left-right', tree.root_node.left.right.title
   end
 
   def test_from_project_specs
     tree = BinarySearchTree.new
     tree.insert(61, "Bill & Ted's Excellent Adventure")
-    tree.insert(16, "Johnny English")
-    tree.insert(92, "Sharknado 3")
-    tree.insert(50, "Hannibal Buress: Animal Furnace")
+    tree.insert(16, 'Johnny English')
+    tree.insert(92, 'Sharknado 3')
+    tree.insert(50, 'Hannibal Buress: Animal Furnace')
     assert_equal "Bill & Ted's Excellent Adventure", tree.root_node.title
-    assert_equal "Johnny English", tree.root_node.left.title
-    assert_equal "Hannibal Buress: Animal Furnace", tree.root_node.left.right.title
+    assert_equal 'Johnny English', tree.root_node.left.title
+    assert_equal 'Hannibal Buress: Animal Furnace', tree.root_node.left.right.title
   end
 
   def test_include_one_single_item_returns_only_root
@@ -75,9 +74,9 @@ class DateNightTest < Minitest::Test
   def test_include_many_items
     tree = BinarySearchTree.new
     tree.insert(61, "Bill & Ted's Excellent Adventure")
-    tree.insert(16, "Johnny English")
-    tree.insert(92, "Sharknado 3")
-    tree.insert(50, "Hannibal Buress: Animal Furnace")
+    tree.insert(16, 'Johnny English')
+    tree.insert(92, 'Sharknado 3')
+    tree.insert(50, 'Hannibal Buress: Animal Furnace')
     assert_equal true, tree.include?(50)
   end
 
@@ -85,35 +84,34 @@ class DateNightTest < Minitest::Test
     tree = BinarySearchTree.new
     tree.insert(61, "Bill & Ted's Excellent Adventure")
     assert_equal true, tree.include?(61)
-    refute tree.include?(14), "this should return false"
+    refute tree.include?(14), 'this should return false'
   end
 
   def test_include_false_returns_false_with_many_items
     tree = BinarySearchTree.new
     tree.insert(61, "Bill & Ted's Excellent Adventure")
-    tree.insert(16, "Johnny English")
-    tree.insert(92, "Sharknado 3")
-    tree.insert(50, "Hannibal Buress: Animal Furnace")
+    tree.insert(16, 'Johnny English')
+    tree.insert(92, 'Sharknado 3')
+    tree.insert(50, 'Hannibal Buress: Animal Furnace')
     assert_equal true, tree.include?(61)
-    refute tree.include?(14), "this should return false"
+    refute tree.include?(14), 'this should return false'
   end
 
   def test_root_node_has_depth_of_0
     tree = BinarySearchTree.new
     tree.insert(61, "Bill & Ted's Excellent Adventure")
-    tree.insert(16, "Johnny English")
-    tree.insert(92, "Sharknado 3")
-    tree.insert(50, "Hannibal Buress: Animal Furnace")
+    tree.insert(16, 'Johnny English')
+    tree.insert(92, 'Sharknado 3')
+    tree.insert(50, 'Hannibal Buress: Animal Furnace')
     assert_equal 0, tree.depth_of(61)
   end
 
   def test_non_root_node_has_depth_of_1
     tree = BinarySearchTree.new
     tree.insert(61, "Bill & Ted's Excellent Adventure")
-    tree.insert(16, "Johnny English")
-    tree.insert(92, "Sharknado 3")
-    tree.insert(50, "Hannibal Buress: Animal Furnace")
+    tree.insert(16, 'Johnny English')
+    tree.insert(92, 'Sharknado 3')
+    tree.insert(50, 'Hannibal Buress: Animal Furnace')
     assert_equal 1, tree.depth_of(92)
   end
-
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -5,26 +5,23 @@ require_relative '../lib/node'
 
 class NodeTest < Minitest::Test
   def test_can_create_object
-    node = Node.new(1, "movie title")
+    node = Node.new(1, 'movie title')
     assert_instance_of Node, node
   end
 
   def test_node_has_score_and_title
-    node = Node.new(1, "movie title")
+    node = Node.new(1, 'movie title')
     assert_equal 1, node.score
-    assert_equal "movie title", node.title
+    assert_equal 'movie title', node.title
   end
 
   def test_node_can_have_left_right_nodes
-    node1 = Node.new(10, "movie title")
-    node2 = Node.new(5, "other title")
-    node3 = Node.new(15, "another title")
+    node1 = Node.new(10, 'movie title')
+    node2 = Node.new(5, 'other title')
+    node3 = Node.new(15, 'another title')
     node1.right = node2
     node1.left =  node3
     assert_equal node1.right, node2
     assert_equal node1.left, node3
   end
-
-
-
 end
